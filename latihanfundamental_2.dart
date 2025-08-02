8eddbded04cb6ce108f6e81543ef3e39d47fc838
kresnosuciarinugroho <ksaflutter@gmail.com>
2025-08-02 15:28:34 +0700

    tipe set dan list

--- latihanfundamental_2.dart
+++ latihanfundamental_2.dart
@@ -2,11 +2,15 @@ void main() {
   // Set untuk menyimpan angka unik
   print("set angka unik");
   Set<int> angkaUnik = {1, 2, 2, 3};
+  // Menyimpan data yang harus unik, tidak peduli urutan, tidak bisa ada data yang sama.
+
+  // Set otomatis menghilangkan duplikat
   print(angkaUnik); // {1, 2, 3}
 
   // List untuk menyimpan nilai ujian
   print("\n\nlist nilai ujian");
   List<int> nilaiUjian = [80, 90, 80, 100];
+  // List menyimpan data berurutan, bisa ada data yang sama, bisa diakses dengan indeks
   print(nilaiUjian); // [80, 90, 80, 100]
   print(nilaiUjian[1]); // 90 (akses data pakai indeks)
 
