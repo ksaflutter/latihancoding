46d5f2de716594976507ae9be02e2f4a624f033b
kresnosuciarinugroho <ksaflutter@gmail.com>
2025-08-02 15:06:41 +0700

    String dengan karakter dan lainnya

--- latihanfundamental_1.dart
+++ latihanfundamental_1.dart
@@ -4,13 +4,41 @@ void main() {
   // dan variabel
   print("\n\nTugas Belajar Fundamental Dart");
   int umur = 48;
-  var nama = "Budi";
+  var nama = "Nugi";
   print("nama saya = $nama"); // menggunakan string interpolation
   print("umur saya = $umur"); // & disini menampilkan data variabel umur
 
   // tugas 1 - menampilkan tipe data string
-  print("\n\n\Tugas 1 - menampilkan bilangan ganjil");
+  print("\n\n\Tugas 1 - menampilkan String interpolasi");
 
   String namaLengkap = "Kresno Suci A";
   print("nama saya : $namaLengkap");
+
+  // tugas 2 - menampilkan tipe data string - pakai string non variable
+  print("\n\nTugas 2 - menampilkan String lower upper case");
+
+  String namaKecil = "nugi";
+  String namaBesar = "Kresno";
+  print("nama kecil : ${namaKecil.toLowerCase()}"); // buka dengan to
+  print("nama besar : ${namaBesar.toUpperCase()}");
+
+  // tugas 3 - menampilkan tipe data string - pakai string non variable
+  print("\n\nTugas 3 - menampilkan String dengan karakter khusus");
+
+  String teks = "Kresno Suci A";
+  print("teks : $teks");
+  print("teks dengan karakter khusus : ${teks.replaceAll(' ', '_')}");
+  // buka dengan replaceAll
+
+  // tugas 4 - menampilkan tipe data string - karakter khusus dan uppercase
+  print(
+    "\n\nTugas 4 - menampilkan String dengan karakter khusus gabung uppercase",
+  );
+
+  String tulisan = "Abu Djalaluddin";
+  print("tulisan : $tulisan");
+  print(
+    "tulisan dengan karakter dan upper : ${tulisan.replaceAll(' ', '_').toUpperCase()}",
+  );
+  // Abu_Djalaluddin
 }
