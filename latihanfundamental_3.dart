80dcd766b85c68e0c126805967914b2f5e7e2db2
kresnosuciarinugroho <ksaflutter@gmail.com>
2025-08-02 17:33:35 +0700

    bool hanya true false aja

---
+++ latihanfundamental_3.dart
@@ -0,0 +1,13 @@
+void main() {
+  // tugas 1 - menampilkan tipe data boolean
+  var name = "Nugi Kresno";
+  bool isMale = true;
+  print("$name adalah laki-laki: $isMale"); // Nugi
+
+  var nameHer = "Sinta Hapsari";
+  bool isHer = false;
+  print(
+    "$nameHer adalah laki-laki: $isHer",
+  ); /* Sinta Hapsari adalah laki-laki: false
+  jadi apapun namanya boleh saja mau isHer mau perempuan atau laki-laki*/
+}
