c521d49d33dc7a96ee6da8359c997ce168e23585
kresnosuciarinugroho <ksaflutter@gmail.com>
2025-08-02 12:40:10 +0700

    Latihan type dalam  dart

---
+++ latihanfundamental_1.dart
@@ -0,0 +1,16 @@
+void main() {
+  // tugas belajar fundamental dart
+  // menampilkan string expresion
+  // dan variabel
+  print("\n\nTugas Belajar Fundamental Dart");
+  int umur = 48;
+  var nama = "Budi";
+  print("nama saya = $nama"); // menggunakan string interpolation
+  print("umur saya = $umur"); // & disini menampilkan data variabel umur
+
+  // tugas 1 - menampilkan tipe data string
+  print("\n\n\Tugas 1 - menampilkan bilangan ganjil");
+
+  String namaLengkap = "Kresno Suci A";
+  print("nama saya : $namaLengkap");
+}
