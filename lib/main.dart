1736c18844f69b2b3d9a1a4938e514ab8ccc8781
kresnosuciarinugroho <ksaflutter@gmail.com>
2025-08-04 14:18:28 +0700

    tugas tiga flutter push

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ppkd_b_3/hari7/tugasflutter_3.dart';
 import 'package:ppkd_b_3/hari7/tugasflutter_4.dart';
 
 void main() {
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: TugasFlutterempat(),
+      home: TugasFluttertiga(),
     );
   }
 }
