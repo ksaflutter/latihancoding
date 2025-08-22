feba26579edc81f27ce3c6769a35e5388286ea15
kresnosuciarinugroho <ksaflutter@gmail.com>
2025-08-22 08:36:09 +0700

    App login satu

---
+++ lib/app_login/home/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppLoginSatu extends StatelessWidget {
+  const AppLoginSatu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
